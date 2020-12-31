301a5e22e743c716124d3c762ec8c64e920b7028
egret678 <scarletoblivion7@gmail.com>
2020-12-31 14:23:35 +0900

    modify a ABC144 c.cpp

--- AtCoder/ABC144/c.cpp
+++ AtCoder/ABC144/c.cpp
@@ -1,25 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
+#define INF32 2147483647 //2.147483647×10^{9}:32bit整数のinf
+#define INF64 9223372036854775807 //9.223372036854775807×10^{18}:64bit整数のinf
+#define MOD 1000000007
 
-int main() {
-  long long N;
-  cin >> N;
-  int i;
-  vector<int> a(sqrt(N));
-  int count = 0;
-  for(i=1; i < sqrt(N); i++){
-    if(N % i ==0){
-      a.at(count) = i;
-      count++;
+ll myceil(ll a,ll b){return (a+(b-1))/b;}
+ll myfloor(ll a,ll b){return a/b;}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n;
+
+    cin >> n;
+
+    ll min_val = INF64;
+    for(ll i = 1; i <= sqrt(n); ++i){
+      if((n % i) == 0){
+        ll i_pair = n / i;
+        if(min_val > (i + i_pair - 2)){
+          min_val = (i + i_pair - 2);
+        }
+      }
     }
-  }
-//  cout << a.at(1) <<endl;
-//  cout << N/a.at(1) <<endl;
-  long long min = a.at(0) + N/a.at(0);
-  for(i=1; i < count; i++){
-    if(a.at(i) + N/a.at(i) < min) min = a.at(i) + N/a.at(i);
-  }
 
-  cout << min-2 <<endl;
+    cout << min_val << '\n';
+
+    return 0;
 }
