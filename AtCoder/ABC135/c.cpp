7b55fc18f90461496d59896ff2ac66ef190ceb46
egret678 <scarletoblivion7@gmail.com>
2021-01-02 23:32:15 +0900

    modify the ABC135 a.cpp

---
+++ AtCoder/ABC135/c.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define INF32 2147483647 //2.147483647×10^{9}:32bit整数のinf
+#define INF64 9223372036854775807 //9.223372036854775807×10^{18}:64bit整数のinf
+#define MOD 1000000007
+
+ll myceil(ll a,ll b){return (a + (b - 1)) / b;}
+ll myfloor(ll a,ll b){return a / b;}
+template<class t,class u> void chmax(t& a,u b){if(a < b) a = b;}
+template<class t,class u> void chmin(t& a,u b){if(b < a) a = b;}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n+1), b(n);
+    int sum = 0;
+    for(auto& e:a){
+        cin >> e;
+        sum += e;
+    }
+    for(auto& e:b) cin >> e;
+
+    int diff_val = 0;
+    for(int i = 0; i < n + 1; ++i){
+        diff_val = a[i] - b[i];
+        if(diff_val > 0){
+            sum -= diff_val;
+        }else{
+            a[i+1] += max(-a[i+1], diff_val);
+        }
+
+    }
+
+    cout << sum << '\n';
+    /*
+    for(const auto& row:a){
+        for(const auto& e:row){
+            cout << 2 * e << " ";
+        }
+        cout << '\n';
+    }
+    */
+    return 0;
+}
