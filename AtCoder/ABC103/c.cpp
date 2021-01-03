d35453616744e800c46455a31cc0b30bdddc8476
egret678 <scarletoblivion7@gmail.com>
2021-01-03 16:42:49 +0900

    solve a ABC103 problem

---
+++ AtCoder/ABC103/c.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define INF32 2147483647 //2.147483647×10^{9}:32bit整数のinf
+#define INF64 9223372036854775807 //9.223372036854775807×10^{18}:64bit整数のinf
+#define MOD 1000000007
+
+ll myceil(ll a,ll b){return (a + (b - 1)) / b;}
+ll myfloor(ll a,ll b){return a / b;}
+template<class t,class u> void chmax(t& a,u b){if(a < b) a = b;}
+template<class t,class u> void chmin(t& a,u b){if(b < a) a = b;}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    ll ans = 0;
+    for(int i = 0; i < n; ++i){
+        int a;
+        cin >> a;
+        ans += (a - 1);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
